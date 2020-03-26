3d3efa8ea97064b94cc02d7ed27c45c5f9473ee1
Roberto Rodriguez <rrodri0622@gmail.com>
2020-03-25 22:56:04 -0400

    Update ala-powershell-data-producer.ps1

--- ala-powershell-data-producer.ps1
+++ ala-powershell-data-producer.ps1
@@ -19,51 +19,53 @@ param (
 
     [Parameter(Mandatory=$true)]
     [ValidateScript({
-        if( -Not ($_ | Test-Path) ){
-            throw "File or folder does not exist"
+        foreach ($f in $_)
+        {
+            if( -Not ($f | Test-Path) ){
+                throw "File or folder does not exist"
+            }
         }
         return $true
     })]
-    [System.IO.FileInfo]$FilePath
+    [string[]]$FilePath,
+    
+    [Parameter(Mandatory=$false)]
+    [switch]$PackMessage
 
 )
 
-# Create two records with the same set of properties to create
-$json = @"
-[{  "StringValue": "MyString1",
-    "NumberValue": 42,
-    "BooleanValue": true,
-    "DateValue": "2019-09-12T20:00:00.625Z",
-    "GUIDValue": "9909ED01-A74C-4874-8ABF-D2678E3AE23D"
-},
-{   "StringValue": "MyString2",
-    "NumberValue": 43,
-    "BooleanValue": false,
-    "DateValue": "2019-09-12T20:00:00.625Z",
-    "GUIDValue": "8809ED01-A74C-4874-8ABF-D2678E3AE23D"
-}]
-"@
+# Aggregate files from input paths
+$all_files = @()
+foreach ($file in $FilePath){
+    if ((Get-Item $file) -is [system.io.fileinfo]){
+        $all_files += (Resolve-Path -Path $file)
+    }
+    elseif ((Get-Item $file) -is [System.IO.DirectoryInfo]){
+        $folderfiles = Get-ChildItem -Path $file -Recurse -Include *.json
+        $all_files += $folderfiles
+    }
+}
 
 # Create the function to create the authorization signature
-Function Build-Signature ($WorkspaceId, $WorkspaceSharedKey, $date, $contentLength, $method, $contentType, $resource)
+Function Build-Signature ($customerId, $sharedkey, $date, $contentLength, $method, $contentType, $resource)
 {
     $xHeaders = "x-ms-date:" + $date
     $stringToHash = $method + "`n" + $contentLength + "`n" + $contentType + "`n" + $xHeaders + "`n" + $resource
 
     $bytesToHash = [Text.Encoding]::UTF8.GetBytes($stringToHash)
-    $keyBytes = [Convert]::FromBase64String($WorkspaceSharedKey)
+    $keyBytes = [Convert]::FromBase64String($sharedkey)
 
     $sha256 = New-Object System.Security.Cryptography.HMACSHA256
     $sha256.Key = $keyBytes
     $calculatedHash = $sha256.ComputeHash($bytesToHash)
     $encodedHash = [Convert]::ToBase64String($calculatedHash)
-    $authorization = 'SharedKey {0}:{1}' -f $WorkspaceId,$encodedHash
+    $authorization = 'SharedKey {0}:{1}' -f $customerId,$encodedHash
     return $authorization
 }
 
 
 # Create the function to create and post the request
-Function Post-LogAnalyticsData($WorkspaceId, $WorkspaceSharedKey, $body, $logType)
+Function Post-LogAnalyticsData($customerId, $sharedkey, $body, $logType)
 {
     $method = "POST"
     $contentType = "application/json"
@@ -71,26 +73,84 @@ Function Post-LogAnalyticsData($WorkspaceId, $WorkspaceSharedKey, $body, $logTyp
     $rfc1123date = [DateTime]::UtcNow.ToString("r")
     $contentLength = $body.Length
     $signature = Build-Signature `
-        -customerId $WorkspaceId `
-        -sharedKey $WorkspaceSharedKey `
+        -customerId $customerId `
+        -sharedKey $sharedkey `
         -date $rfc1123date `
         -contentLength $contentLength `
         -method $method `
         -contentType $contentType `
         -resource $resource
-    $uri = "https://" + $WorkspaceId + ".ods.opinsights.azure.com" + $resource + "?api-version=2016-04-01"
 
-    $headers = @{
-        "Authorization" = $signature;
-        "Log-Type" = $logType;
-        "x-ms-date" = $rfc1123date;
-        "time-generated-field" = $TimeStampField;
+    $uri = "https://" + $customerId + ".ods.opinsights.azure.com" + $resource + "?api-version=2016-04-01"
+
+    if ($TimeStampField.length -gt 0)
+    {
+        $headers = @{
+            "Authorization" = $signature;
+            "Log-Type" = $logType;
+            "x-ms-date" = $rfc1123date;
+            "time-generated-field"=$TimeStampField;
+        }
     }
-
-    $response = Invoke-WebRequest -Uri $uri -Method $method -ContentType $contentType -Headers $headers -Body $body -UseBasicParsing
+    else {
+         $headers = @{
+            "Authorization" = $signature;
+            "Log-Type" = $logType;
+            "x-ms-date" = $rfc1123date;
+        }
+    }
+    $response = Invoke-WebRequest -Uri $uri -Method $method -ContentType $contentType -Headers $headers -Body $body -UseBasicParsing -Verbose
     return $response.StatusCode
 
 }
 
-# Submit the data to the API endpoint
-Post-LogAnalyticsData -customerId $WorkspaceId -sharedKey $WorkspaceSharedKey -body ([System.Text.Encoding]::UTF8.GetBytes($json)) -logType $logType
+$APILimitBytes = 30 * 1mb
+
+foreach ($dataset in $all_files){
+    $total_file_size = (get-item -Path $dataset).Length
+    $json_records = @()
+    $json_current_size = 0
+
+    # Read each JSON object from file
+    foreach($line in [System.IO.File]::ReadLines($dataset))
+    {
+        Start-Sleep -s 0.1
+        $lineSize = [System.Text.ASCIIEncoding]::UTF8.GetByteCount($line)
+        $json_current_size += ($lineSize + 1 )
+
+        Write-Progress -Activity “Processing files” -status "Processing $dataset” -percentComplete ($json_current_size / $total_file_size * 100)
+
+        if ($PackMessage)
+        {
+            $message = @{
+                "message" = $line
+            }
+        }
+        else
+        {
+            $message = $line | ConvertFrom-Json
+        }
+
+        # Maximum of 30MB per post to Azure Monitor Data Collector API
+        if ($json_current_size -lt $APILimitBytes)
+        {
+            $json_records += $message
+        }
+        else
+        {
+            # Submit the data to the API endpoint
+            $json_records = $json_records | ConvertTo-Json
+            Post-LogAnalyticsData -customerId $WorkspaceId -sharedKey $WorkspaceSharedKey -body ([System.Text.Encoding]::UTF8.GetBytes($json_records)) -logType $logType
+            $json_records = @()
+            $json_current_size = 0
+        }
+
+        # if you get to read the whole file without reaching the 30MB limit per post
+        if ($json_current_size -eq $total_file_size)
+        {
+            # Submit the data to the API endpoint
+            $json_records = $json_records | ConvertTo-Json
+            Post-LogAnalyticsData -customerId $WorkspaceId -sharedKey $WorkspaceSharedKey -body ([System.Text.Encoding]::UTF8.GetBytes($json_records)) -logType $logType   
+        }
+    }
+}
